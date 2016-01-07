7eeffa4cf4414096c3d3bf0d91c6d6ed005379a8
Wiiz83 <uzanlah@gmail.com>
2016-01-07 12:53:53 +0100

    Create AffichageTableauSymboles.c

---
+++ AffichageTableauSymboles.c
@@ -0,0 +1,52 @@
+#include "lectureEnTeteSection.h"
+
+/* 
+TODO 
+
+
+*/
+
+void getTableauSymboles(FILE* f, Elf32_Ehdr header_elf, Elf32_Shdr section_elf) 
+{
+
+	Elf32_Sym *symboltable;
+	int i;
+	unsigned int symboltable_size = 0; 
+	fseek(f, symboltable_size, SEEK_SET);
+
+	while(symboltable[i] != '\0')
+	{
+
+		fseek(f, symboltable_size + i, SEEK_SET);
+		symboltable = (Elf32_Sym*) malloc(sizeof(Elf32_Sym)+i);
+
+		if(symboltable[i] == ELFDATA2LSB)
+		{
+			
+			fread(&symbole_elf.st_name, sizeof(uint32_t), 1, f);
+		} 
+
+		else if(header_elf.e_ident[5] == ELFDATA2MSB)
+		{
+
+			fread(&symbole_elf.st_name, sizeof(uint32_t), 1, f);
+			symbole_elf.st_name = __bswap_32(symbole_elf.st_name);
+
+		}
+
+		else
+		{
+			printf("Section au format de donnée inconnu");
+			exit(0);
+		}
+
+		t[i] = symbole_elf;
+
+		}
+
+		symboltable_size += 1;
+	
+	}
+
+
+}
