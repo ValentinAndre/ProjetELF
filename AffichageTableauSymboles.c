69aeb267a055875da713e2fddb80ef29860c442b
Wiiz83 <uzanlah@gmail.com>
2016-01-07 13:02:50 +0100

    Update AffichageTableauSymboles.c

--- AffichageTableauSymboles.c
+++ AffichageTableauSymboles.c
@@ -10,28 +10,24 @@ void getTableauSymboles(FILE* f, Elf32_Ehdr header_elf, Elf32_Shdr section_elf)
 {
 
 	Elf32_Sym *symboltable;
-	int i;
-	unsigned int symboltable_size = 0; 
+	int i = 0, symboltable_size = 0;
 	fseek(f, symboltable_size, SEEK_SET);
 
 	while(symboltable[i] != '\0')
 	{
 
-		fseek(f, symboltable_size + i, SEEK_SET);
+		fseek(f, symboltable_size, SEEK_SET);
 		symboltable = (Elf32_Sym*) malloc(sizeof(Elf32_Sym)+i);
 
 		if(symboltable[i] == ELFDATA2LSB)
 		{
-			
 			fread(&symbole_elf.st_name, sizeof(uint32_t), 1, f);
 		} 
 
 		else if(header_elf.e_ident[5] == ELFDATA2MSB)
 		{
-
 			fread(&symbole_elf.st_name, sizeof(uint32_t), 1, f);
 			symbole_elf.st_name = __bswap_32(symbole_elf.st_name);
-
 		}
 
 		else
@@ -40,13 +36,12 @@ void getTableauSymboles(FILE* f, Elf32_Ehdr header_elf, Elf32_Shdr section_elf)
 			exit(0);
 		}
 
-		t[i] = symbole_elf;
-
-		}
-
+		symboltable[i] = symbole_elf;
 		symboltable_size += 1;
-	
+		i += 1;
 	}
-
-
+	
 }
+
+
+
